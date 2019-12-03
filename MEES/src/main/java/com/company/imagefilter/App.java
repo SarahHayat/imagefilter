6514a4ab8b1db959c435fa6e08b53aa06d212f79
marine <marine.b.58@hotmail.fr>
2019-12-03 19:31:18 +0100

    parser with error
    help Robin !

--- MEES/src/main/java/com/company/imagefilter/App.java
+++ MEES/src/main/java/com/company/imagefilter/App.java
@@ -1,7 +1,43 @@
 package com.company.imagefilter;
 
-public class App {
-    public static void main(String[] args) {
+
+public class App
+{
+    public static void parser(String[] args)
+    {
+        //options
+        Options options = new Options();
+
+        options.addOption("--filters", true, "Filter to applicate");
+        options.addOption("-i", true, "File with picture source");
+        options.addOption("-o", true, "File with picture modificate");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+
+        String fileModificate = "output";
+        String file = "toModificate";
+        String filters = null;
+        if (cmd.hasOption("-i")) {
+            file = cmd.getOptionValue("-i");
+            // retrieve the images from the folder
+        }
+        if (cmd.hasOption("-o"))
+        {
+           fileModificate = cmd.getOptionValue("-o");
+            // file for put the picture change
+        }
+        if (cmd.hasOption("--filters"))
+        {
+            filters = cmd.getOptionValue("--filters");
+            // filters without parse
+        }
+        treatment(file, fileModificate, filters);
+    }
+
+    public static void main(String[] args)
+    {
         System.out.println("hello");
+        parser(args);
     }
 }
