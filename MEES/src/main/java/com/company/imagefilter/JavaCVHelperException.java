5fdaa77a381bc7d1aa6398aa8cc28f6e4015aa66
SarahHayat <saraahyt@gmail.com>
2019-12-04 09:41:33 +0100

    Auto stash before merge of "marine" and "origin/marine"

--- MEES/src/main/java/com/company/imagefilter/JavaCVHelperException.java
+++ MEES/src/main/java/com/company/imagefilter/JavaCVHelperException.java
@@ -2,10 +2,12 @@ package com.company.imagefilter;
 
 public class JavaCVHelperException extends Exception {
     public JavaCVHelperException(String message) {
+
         super(message);
     }
 
     public JavaCVHelperException(String message, Throwable cause) {
+
         super(message, cause);
     }
 }
