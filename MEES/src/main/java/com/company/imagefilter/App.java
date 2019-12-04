d291a706a7dc0930cab65d245606a350c16835c6
marine <marine.b.58@hotmail.fr>
2019-12-04 11:17:49 +0100

    parser without file

--- MEES/src/main/java/com/company/imagefilter/App.java
+++ MEES/src/main/java/com/company/imagefilter/App.java
@@ -2,16 +2,17 @@ package com.company.imagefilter;
 
 
 import org.apache.commons.cli.*;
+import java.lang.String;
 
 public class App
 {
     public static void parser(String[] args) throws ParseException {
         //options
-        Options options = new Options();
+       Options options = new Options();
 
-        options.addOption("--filters", true, "Filter to applicate");
-        options.addOption("-i", true, "File with picture source");
-        options.addOption("-o", true, "File with picture modificate");
+        options.addOption("f", "filters", true, "filters");
+        options.addOption("i", true, "File with picture source");
+        options.addOption("o", true, "File with picture modificate");
 
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = parser.parse(options, args);
@@ -19,32 +20,30 @@ public class App
         String fileModificate = "output";
         String file = "toModificate";
         String filters = null;
-        if (cmd.hasOption("-i")) {
-            file = cmd.getOptionValue("-i");
+        if (cmd.hasOption("i")) {
+            file = cmd.getOptionValue("i");
             // retrieve the images from the folder
         }
-        if (cmd.hasOption("-o"))
+        if (cmd.hasOption("o"))
         {
-           fileModificate = cmd.getOptionValue("-o");
+           fileModificate = cmd.getOptionValue("o");
             // file for put the picture change
         }
-        if (cmd.hasOption("--filters"))
+        if (cmd.hasOption("f"))
         {
-            filters = cmd.getOptionValue("--filters");
+            filters = cmd.getOptionValue("f");
             // filters without parse
         }
-<<<<<<< Updated upstream
-     //   treatment(file, fileModificate, filters);
-=======
-        System.out.println("file = " + file + "fileModificate = " + fileModificate + "filters = " + filters);
+        System.out.println("file = " + file + " fileModificate = " + fileModificate + " filters = " + filters);
       // treatment(file, fileModificate, filters);
->>>>>>> Stashed changes
     }
 
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
         System.out.println("hello");
-        parser(args);
-
-
+        try {
+            parser(args);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
