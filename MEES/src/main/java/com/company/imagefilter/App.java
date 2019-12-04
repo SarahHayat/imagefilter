f414c5062a5dd5f7a72445565ca8bbbe5d15a9cb
EMERICKCHALET <emerickchalet@gmail.com>
2019-12-04 11:28:45 +0100

    Auto stash before rebase of "origin/marine"

--- MEES/src/main/java/com/company/imagefilter/App.java
+++ MEES/src/main/java/com/company/imagefilter/App.java
@@ -1,11 +1,23 @@
 package com.company.imagefilter;
+import org.apache.commons.cli.*;
 
+import java.sql.SQLOutput;
 
+<<<<<<< Updated upstream
 import org.apache.commons.cli.*;
 import java.lang.String;
+=======
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        try {
+            parser(args);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+>>>>>>> Stashed changes
 
-public class App
-{
     public static void parser(String[] args) throws ParseException {
         //options
        Options options = new Options();
@@ -24,6 +36,7 @@ public class App
             file = cmd.getOptionValue("i");
             // retrieve the images from the folder
         }
+<<<<<<< Updated upstream
         if (cmd.hasOption("o"))
         {
            fileModificate = cmd.getOptionValue("o");
@@ -45,5 +58,17 @@ public class App
         } catch (ParseException e) {
             e.printStackTrace();
         }
+=======
+        if (cmd.hasOption("-o")) {
+            fileModificate = cmd.getOptionValue("-o");
+            // file for put the picture change
+        }
+        if (cmd.hasOption("--filters")) {
+            filters = cmd.getOptionValue("--filters");
+            // filters without parse
+        }
+        //   treatment(file, fileModificate, filters);
+>>>>>>> Stashed changes
     }
 }
+
