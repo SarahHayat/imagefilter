6b0e29d9914979dbf411db0e7bf8e5ea02afc17c
Erwan <erwan.spenette@icloud.com>
2019-12-05 11:56:28 +0100

    All changes

--- MEES/src/main/java/com/company/imagefilter/App.java
+++ MEES/src/main/java/com/company/imagefilter/App.java
@@ -73,20 +73,17 @@ public class App {
                                     image = f. process(image, argc, String.valueOf(listFile[i]));
                                 }
 
-                                // Mat result = bw.filterGrayscale(image, l);
-                               // String[] splitFile = filterArg.split("\\/");
                                 String nameFile = listFile[i].getName();//.split(splitFile);
-                                System.out.println("nameFile = " +nameFile + "output =" + output);
 
                                 File outputFile = new File(outputDir, nameFile);
 
                                 opencv_imgcodecs.imwrite(outputFile.getAbsolutePath(), image);
                             } catch (JavaCVHelperException e) {
                                 e.printStackTrace();
-                                System.out.println("le filtre n'a pas pu etre appliqué");
+                                System.out.println("The application of filter work");
                             }
                         }
-                        System.out.println("L'application du filtre à bien marché");
+                        System.out.println("The application of filter doesn't work");
                     }
                 }
             }
@@ -142,7 +139,6 @@ public class App {
                 if (filterAdd != null)
                     filters = String.valueOf(filterAdd);
             }
-
         }
         else
             System.out.println("file not found");
--- MEES/src/main/java/com/company/imagefilter/Blur.java
+++ MEES/src/main/java/com/company/imagefilter/Blur.java
@@ -6,18 +6,16 @@ import org.bytedeco.opencv.opencv_core.Size;
 import static org.bytedeco.opencv.global.opencv_imgproc.GaussianBlur;
 
 public class Blur extends Filter  {
-//    public Mat filterBlur(Mat image, Log l) throws JavaCVHelperException  {
-//
 
     @Override
     Mat process(Mat image, int args, String file) throws JavaCVHelperException {
             Mat result = image.clone();
             GaussianBlur(image, result, new Size(args, args), 0);
-
-            App.l.Log(" image " + file + "\n" + "filtre : " + Blur.class.getSimpleName() + "\n");
+        System.out.println(" image :" + file + "\n" + "filter : " + Blur.class.getSimpleName() + "\n");
+            App.l.Log(" image " + file + "\n" + "filter : " + Blur.class.getSimpleName() + "\n");
 
             if (result == null) {
-                throw new JavaCVHelperException("le flou ne marche pas");
+                throw new JavaCVHelperException("The blur doesn't work");
 
             }return result;
         }
